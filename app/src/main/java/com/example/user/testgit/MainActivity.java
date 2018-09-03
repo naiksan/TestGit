9d566ee500431ca7a8603c081c26a5e5209060f7
Sanket Naik <naiksan64@gmail.com>
2018-09-03 11:31:58 +0530

    removed call

--- app/src/main/java/com/example/user/testgit/MainActivity.java
+++ app/src/main/java/com/example/user/testgit/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG,"onCreate()");
         showToast();
         showToast();
-        showToast();
     }
 
     private void showToast(){
