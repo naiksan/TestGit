8f7dfda55b51ca5e719b718c497d8a261de7cbbc
Sanket Naik <naiksan64@gmail.com>
2018-09-03 09:39:29 +0530

    log added

--- app/src/main/java/com/example/user/testgit/MainActivity.java
+++ app/src/main/java/com/example/user/testgit/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.user.testgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+        Log.d(TAG,"onCreate()");
     }
 }
